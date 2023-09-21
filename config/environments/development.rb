e90c378437584ee0e70f73181b23fcf41d9a7ad6
cliff <cliffmudigo5@gmail.com>
2023-09-21 15:34:48 +0300

    navbar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -56,6 +57,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+
+ 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -64,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
 end
